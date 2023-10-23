bd8d01c9348d78fefaa38dac00e54915d7f9b674
Haidarov <nozimjonhaydarov6@gmail.com>
2023-10-23 20:58:58 +0500

    Initial Commit

---
+++ SwiftUI-wearher/SwiftUI_wearherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_wearherApp.swift
+//  SwiftUI-wearher
+//
+//  Created by Haidarov N on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_wearherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
