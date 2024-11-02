2f217e74abbf3d4e4ffca05f2391efc2c86abe88
haidarov_n <nozimjonhaydarov6@gmail.com>
2024-11-02 12:05:00 +0500

    u

--- SwiftUI-wearherTests/SwiftUI_weatherTests.swift
+++ SwiftUI-wearherTests/SwiftUI_weatherTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import SwiftUI_wearher
 
-final class SwiftUI_wearherTests: XCTestCase {
+final class SwiftUI_weatherTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- SwiftUI-weatherUITests/SwiftUI_weatherUITests.swift
+++ SwiftUI-weatherUITests/SwiftUI_weatherUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class SwiftUI_wearherUITests: XCTestCase {
+final class SwiftUI_weatherUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
